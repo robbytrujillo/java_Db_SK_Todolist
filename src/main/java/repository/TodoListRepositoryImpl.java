68a3421f36fbd03c58881dfa991e88ae81dd0b68
Robby Ilhamkusuma <robbyikusuma88@gmail.com>
2023-11-03 06:37:49 +0700

    Java Database | Repository-Menghapus-Todolist

--- src/main/java/repository/TodoListRepositoryImpl.java
+++ src/main/java/repository/TodoListRepositoryImpl.java
@@ -5,6 +5,7 @@ import entity.Todolist;
 import javax.sql.DataSource;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 
 public class TodoListRepositoryImpl implements TodoListRepository {
@@ -73,23 +74,55 @@ public class TodoListRepositoryImpl implements TodoListRepository {
         }
     }
 
+    private boolean isExists(Integer number) {
+        String sql = "SELECT id FROM todolist WHERE id = ?";
+        try(Connection connection = dataSource.getConnection();
+        PreparedStatement statement = connection.prepareStatement(sql)) {
+        statement.setInt(1, number);
 
+        try (ResultSet resultSet = statement.executeQuery()){
+            if (resultSet.next()) {
+                return true;
+            } else {
+                return false;
+            }
+        }
+
+        } catch (SQLException exception) {
+            throw new RuntimeException(exception);
+        }
+    }
 
     @Override
     public boolean remove(Integer number) {
-        if ((number - 1) >= data.length){
-            return false;
-        }else if (data[number - 1] == null) {
-            return false;
-        }else {
-            for (int i = (number - 1); i < data.length; i++) {
-                if (i == (data.length - 1)) {
-                    data[i] = null;
-                } else {
-                    data[i] = data[i + 1];
-                }
+//        if ((number - 1) >= data.length){
+//            return false;
+//        }else if (data[number - 1] == null) {
+//            return false;
+//        }else {
+//            for (int i = (number - 1); i < data.length; i++) {
+//                if (i == (data.length - 1)) {
+//                    data[i] = null;
+//                } else {
+//                    data[i] = data[i + 1];
+//                }
+//            }
+//            return true;
+//        }
+        if(isExists(number)) {
+            String sql = "DELETE FROM todolist WHERE id = ?";
+
+            try (Connection connection = dataSource.getConnection();
+                 PreparedStatement statement = connection.prepareStatement(sql)) {
+                statement.setInt(1, number);
+                statement.executeUpdate();
+
+                return true;
+            } catch (SQLException exception) {
+                throw new RuntimeException(exception);
             }
-            return true;
+        } else {
+            return false;
         }
     }
 }
